b76f7605304bf7375212a9bc5d636838df0be2bc
Makarova Darya <makarovadarya2704@gmail.com>
2022-10-28 23:55:11 +0300

    Add project files.

---
+++ Homework/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public char age { get; set; }
+    }
+    public class employee : Person
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
