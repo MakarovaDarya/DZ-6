9486cf0e5043828bb862c63ebf115349d214240e
Makarova Darya <makarovadarya2704@gmail.com>
2022-12-23 15:16:20 +0300

    oop

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,24 +1,204 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Homework
 {
+    abstract class Employee
+    {
+        public string name { get; }
+        public Employee(string name)
+        {
+            this.name = name;
+        }
+        public void Work() => Console.WriteLine($"{name} на работе");
+    }
+    
     public class Person
     {
         public string name { get; set; }
-        public char age { get; set; }
+        public string surname { get; set; }
+        public int age { get; set; }
+        public Person (string name,string surname,int age)
+        {
+            this.name = name;   
+            this.surname = surname; 
+            this.age = age; 
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Имя \t{name}\nФамилия \t{surname}\nВозраст \t{age}");
+        }
     }
-    public class employee : Person
+    public class JobTitle : Person
     {
+        private int check { get; set; }
+        private string inn { get;  }
+        private string snils { get; }
+        public string education { get; }
+        public JobTitle(string name, string surname, int age, int check,string inn,string snils,string education)
+            : base(name, surname, age)
+        {
+            this.check = check;
+            this.inn = inn; 
+            this.snils = snils;
+            this.education= education;  
+        }
+    }
+    public class Client : Person
+    {
+        private int budget { get; }
+        public string car { get; }
+        public Client(string name, string surname, int age,int budget, string car)
+            : base(name, surname, age)
+        {
+            this.budget = budget;
+            this.car = car;
+        }   
+    }
+    public class mechanic : JobTitle
+    {
+        
+        public mechanic(string name, string surname, int age, int check, string inn, string snils, string education)
+            : base(name, surname, age,check,  inn,  snils,  education) { }
+        
+    }
+    public class Chief: JobTitle
+    {
+        public Chief(string name, string surname, int age, int check, string inn, string snils, string education)
+            : base(name, surname, age, check, inn, snils, education) { }
+    }
+    
+    public class ServicesAndPrices
+    {
+        public void Services(List<string> services)
+        {
+            StreamReader list = new StreamReader("Services.txt");
+            string line;
+            while ((line = list.ReadLine()) != null)
+            {
+                services.Add(line);
+            }
+        }
+        public void PrintServices()
+        {
+            List<string> services = new List<string>();
+            Services(services);
+            foreach(string it in services)
+                Console.WriteLine(it);
+        }
+        public void Prices(ref List<string> prices)
+        {
+            string file = File.ReadAllText("Prices.txt");
+            int[] nums = file
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(n => int.Parse(n))
+                .ToArray();
+            foreach (int i in nums)
+                Console.WriteLine(i);
+        }
+        public void PrintPrices()
+        {
+            List<string> prices = new List<string>();
+            Services(prices);
+            foreach (string it in prices)
+                Console.WriteLine(it);
+        }
+    }
+   
+    public class accountant:JobTitle
+    {
+        public accountant(string name, string surname, int age, int check, string inn, string snils, string education)
+            : base(name, surname, age, check, inn, snils, education) { }
+
+
+        public class CarService
+        {
+            public int arenda;
+            public int electrSupply;
+            public int equipment;
+            public int purchase;
+            public CarService(int arenda, int electrSupply, int equipment, int purchase)
+            {
+                this.arenda = arenda;
+                this.electrSupply = electrSupply;
+                this.equipment = equipment;
+                this.purchase = purchase;
+            }
+            public void CarServiceCosts()
+            {
+                CarService salon = new CarService(250000, 74000, 67000, 3000);
+                Console.WriteLine($" Месячные траты на автосерис составляют - {arenda + electrSupply + equipment + purchase}");
+            }
+        }
+    }
+    class visitor: Person
+    {
+        public visitor(string name,string surname,int age) : base(name,surname,age) { }
+        public void GetInfo(StreamReader sr,ref List<string> people)
+        {
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                people.Add(line);
+            }
+        }
+        public void NewVisitor(ref List<string> people)
+        {
+            Console.WriteLine("Здравствуйте, я Валерий");
+            string nameV = Console.ReadLine();
+            Console.WriteLine("Сколько людей находится в автосалоне: ");
+            foreach (string person in people)
+            {
+                Console.WriteLine(person);
+            }
+            Console.WriteLine("Поговрите с новым клиентом?");
+            string answ = Console.ReadLine();
+            switch (answ)
+            {
+                case "да":
+                    Console.WriteLine("Какая у вас проблема?");
+                    int problem = int.Parse(Console.ReadLine());
+                    /*
+                 * 1. Замена тормозных колодок
+                 * 2. Утечка охлаждающей жикости
+                 * 3. Царапины
+                 * 4. Вмятины
+                 * 5. Замена шин
+                 * 6. Проблемы с аккумулятором
+                 * 7. Топливный насос
+                 * 8. Кондиционер
+                 * 9. Свчеи и катушки зажигания
+                */
+                    break;
+                case "нет":
+                    Console.WriteLine("Эти клиенты все еще ждут");
+                    foreach (string person in people)
+                    {
+                        Console.WriteLine(person);
+                    }
+                    break;
+            }
+        }
 
     }
+    
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Сегодня отличный рабочий день");
+            accountant Ivan = new accountant("Иван","Иванов", 23, 12342,"657484","5757674544","Механик");
+            Console.WriteLine("Работа бухгалтера выполнена: ", Ivan);
+
+            visitor newChelovek = new visitor("Евгений","Михайлов",32);
+            Console.WriteLine("Зашел новый клиент: ",newChelovek);
         }
     }
 }
--- Tumakov/Program.cs
+++ Tumakov/Program.cs
@@ -1,15 +1,147 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Tumakov
 {
-    internal class Program
+    enum AccountType
     {
-        static void Main(string[] args)
+        Текущий,
+        Сберегательный
+    }
+    class Bank
+    {
+        public Bank(string accountNumber, double balance, AccountType type)
         {
+            this.accountNumber = accountNumber;
+            this.balance = balance;
+            this.type = type;
+        }
+        private string accountNumber { get; set; }
+        public double balance { get; set; }
+        public AccountType type { get; set; }
+        public override string ToString() => $"Номер счета - {accountNumber}, Баланс - {balance}, Тип банковского счета - {type}";
+
+        public double PutOn(double balance)
+        {
+            Console.Write("Введите сумму для пополнения ");
+            double temp = Convert.ToDouble(Console.ReadLine());
+            return balance + temp;
+        }
+        public double WithdrawFrom(double balance)
+        {
+            Console.Write("Введите сумму, которую хотите снять: ");
+            double temp = Convert.ToDouble(Console.ReadLine());
+            if (balance >= temp)
+            {
+                return balance - temp;
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно средств");
+                return balance;
+            }
+        }
+
+    }
+    class Building
+    {
+        private int numbe;
+        public int Numb { get=> numbe; set => numbe = value; }
+        
+        private ushort height;
+        public ushort Height { get => height; set => height = value; }
+        
+        private byte numberOfFloors;
+        public byte floors { get => numberOfFloors; set => numberOfFloors = value; }
+        
+        private ushort numberOfFlats;
+        public ushort Flats
+        {
+            get => numberOfFlats;
+            set => numberOfFlats = value;
+        }
+        private byte numberOfEntrance;
+        public byte Entrance
+        {
+            get => numberOfEntrance;
+            set => numberOfEntrance = value;
+        }
+        static Random rand = new Random();
+        public static int number = rand.Next(1000);
+        public Building(int n, ushort h, byte Floors, ushort Flats, byte entrances)
+        {
+            this.numbe = n;
+            this.height = h;
+            this.numberOfFloors = Floors;
+            this.numberOfFlats = Flats;
+            this.numberOfEntrance = entrances;
+        }
+        public int floorHeight(ushort h, byte f)
+        {
+            int temp = h / f;
+            return temp;
+        }
+        public int FlatsInEntrance(ushort f, byte e)
+        {
+            int temp = f / e;
+            return temp;
+        }
+        public int FlatsOnFloor(ushort flats, byte e, byte floors)
+        {
+            int temp = (flats / e) / (floors - 1);
+            return temp;
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Задание 7.1");
+            AccountType Type1 = AccountType.Текущий;
+            Console.WriteLine(Type1);
+            Bank bank = new Bank("3387448", 1000, AccountType.Сберегательный);
+            Console.WriteLine(bank);
+
+            Console.WriteLine("Задание 7.2");
+            AccountType Type2 = AccountType.Сберегательный;
+            Console.WriteLine(Type2);
+            Random rnd = new Random();
+            int accNum1 = rnd.Next(1000, 100000);
+            string accnum11 = accNum1.ToString();
+            Bank bank1 = new Bank(accnum11, 5608800, AccountType.Сберегательный);
+            Console.WriteLine(bank1);
+
+            Console.WriteLine("Задание 7.3");
+            Random rnd1 = new Random();
+            int accNum2 = rnd1.Next(1000, 100000);
+            string accNum12 = accNum2.ToString();
+            Bank bank2 = new Bank(accNum12, 108800, AccountType.Текущий);
+            Console.WriteLine("Выберите операцию \n1. Снять\n2. Пополнить");
+
+            int answer = int.Parse(Console.ReadLine());
+            switch (answer)
+            {
+                case 1:
+                    bank2.balance = bank2.WithdrawFrom(bank2.balance);
+                    Console.WriteLine(bank2);
+                    break;
+                case 2:
+                    bank2.balance = bank2.PutOn(bank2.balance);
+                    Console.WriteLine(bank2);
+                    break;
+                default:
+                    Console.WriteLine("Запрос отклонен");
+                    break;
+            }
+
+
+            Console.WriteLine("Домашнее задание");
+            Building building = new Building(1, 45, 18, 120, 7);
+            Console.WriteLine($"Высота одного этажа {building.floorHeight(building.Height, building.floors)} метра, количество квартир в подъезде: {building.FlatsInEntrance(building.Flats, building.Entrance)}, количество квартир на этаже: {building.FlatsOnFloor(building.Flats, building.Entrance, building.floors)}");
         }
     }
 }
